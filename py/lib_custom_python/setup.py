7eeede832f4585940bee1373d3d2daf61fa9e8ea
jader-lima <jaderablima@gmail.com>
2024-11-18 08:12:02 -0300

    first commit

--- py/lib_custom_python/setup.py
+++ py/lib_custom_python/setup.py
@@ -11,20 +11,20 @@ setup(
             ''
         ]),
         install_requires=[
-            'pyspark==3.5.0'
+            'pyspark==3.5.0','delta-spark==3.2.0','pytest'
         ],
 
-    # Metadados
+    # metadata
     author="Seu Nome",
     author_email="seuemail@example.com",
     description="short lib descripton",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/seuusuario/my_library", 
+    url="https://github.com/your_user/your_library", 
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.8.4",  # Versão mínima do Python
+    python_requires=">=3.8.4",  # python version
 )
--- scripts/olist/ingestion/customer_ingestion.py
+++ scripts/olist/ingestion/customer_ingestion.py
@@ -1,21 +1,25 @@
 import os
 os.system("pip install /opt/spark/data/libs/lib_custom_python-0.1.0-py3-none-any.whl")
 os.system("pip freeze")
-from pyspark.sql import SparkSession
-from lib_custom_python.storage_functions import read_text_file  
 
+from pyspark.sql import SparkSession
+from lib_custom_python.storage_functions.storage_functions import read_text_file, write_parquet
 
 # Create spark session
 spark = SparkSession.builder.appName("olist_customer_ingestion").getOrCreate()
 
 path = '/opt/spark/data/olist/olist_customers_dataset.csv'
 
-df_customer = read_text_file( spark, csv, True,True, ',', path,'"', "UTF-8")
-
-#df_customer  = spark.read.format('csv').load('/opt/spark/data/olist/olist_customers_dataset.csv')
+# load csv data to a spark dataframe
+df_customer = read_text_file( spark, 'csv', True,True, ',', path,'"', "UTF-8")
 
 df_customer.printSchema()
-#df_customer.write.format('parquet').mode('overwrite').save('/opt/spark/data/bronze/olist/customers')
+
+path_bronze = '/opt/spark/data/bronze/olist/customers'
+mode = 'overwrite'
+# saving dataframe
+
+write_parquet(df_customer, path_bronze, mode)
 
 # stopping spark session
 spark.stop()
